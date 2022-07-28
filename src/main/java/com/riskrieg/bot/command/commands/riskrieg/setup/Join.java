2e518111da5c30cea247e0fe627843f67480d12b
Aaron J Yoder <aaronjyoder@gmail.com>
2022-07-28 18:53:33 -0400

    Join: Add Spanish localization

--- src/main/java/com/riskrieg/bot/command/commands/riskrieg/setup/Join.java
+++ src/main/java/com/riskrieg/bot/command/commands/riskrieg/setup/Join.java
@@ -76,7 +76,8 @@ public class Join implements Command {
         .setGuildOnly(true)
         .setLocalizationFunction(
             RkLocalizationFunction.fromExternalBundles(this,
-                DiscordLocale.ENGLISH_US
+                DiscordLocale.ENGLISH_US,
+                DiscordLocale.SPANISH
             ).build()
         );
   }
