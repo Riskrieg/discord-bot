505449eb64a0974dc313c07fe6976a8fe0618ea9
Aaron J Yoder <aaronjyoder@gmail.com>
2022-07-28 19:45:09 -0400

    Unally: Add English (US) localization

--- src/main/java/com/riskrieg/bot/command/commands/riskrieg/running/alliances/Unally.java
+++ src/main/java/com/riskrieg/bot/command/commands/riskrieg/running/alliances/Unally.java
@@ -23,6 +23,7 @@ import com.riskrieg.bot.command.Command;
 import com.riskrieg.bot.command.settings.Settings;
 import com.riskrieg.bot.command.settings.StandardSettings;
 import com.riskrieg.bot.util.MessageUtil;
+import com.riskrieg.bot.util.lang.RkLocalizationFunction;
 import com.riskrieg.core.api.Riskrieg;
 import com.riskrieg.core.api.RiskriegBuilder;
 import com.riskrieg.core.api.game.feature.alliance.AllianceStatus;
@@ -37,6 +38,7 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.DiscordLocale;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
@@ -49,7 +51,7 @@ public class Unally implements Command {
 
   public Unally() {
     this.settings = new StandardSettings(
-        "Break or deny an alliance with a player.",
+        "Break or deny an alliance with another player.",
         "unally", "deny")
         .withColor(RkpPalette.DEFAULT_BORDER_COLOR.toAwtColor())
         .makeGuildOnly();
@@ -65,7 +67,12 @@ public class Unally implements Command {
   public CommandData commandData() {
     return Commands.slash(settings().name(), settings().description())
         .addOption(OptionType.USER, "player", "Select the player you would like to break or deny an alliance with.", true)
-        .setGuildOnly(true);
+        .setGuildOnly(true)
+        .setLocalizationFunction(
+            RkLocalizationFunction.fromExternalBundles(this,
+                DiscordLocale.ENGLISH_US
+            ).build()
+        );
   }
 
   @Override
