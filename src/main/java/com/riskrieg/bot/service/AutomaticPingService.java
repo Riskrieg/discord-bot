cb3d2632f282e4c7abf9f6e0b3a31142af273151
Aaron J Yoder <aaronjyoder@gmail.com>
2024-03-03 19:55:26 -0500

    Use apache commons ImmutablePair

--- src/main/java/com/riskrieg/bot/service/AutomaticPingService.java
+++ src/main/java/com/riskrieg/bot/service/AutomaticPingService.java
@@ -7,15 +7,17 @@ import com.riskrieg.core.api.RiskriegBuilder;
 import com.riskrieg.core.api.game.Game;
 import com.riskrieg.core.api.game.GamePhase;
 import com.riskrieg.core.api.group.Group;
-import com.riskrieg.core.api.identifier.GameIdentifier;
-import com.riskrieg.core.api.identifier.GroupIdentifier;
 import com.riskrieg.core.util.io.RkJsonUtil;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -70,12 +72,12 @@ public class AutomaticPingService implements Service {
                                 .filter(c -> c.identifier().equals(game.identifier()))
                                 .findFirst()
                                 .orElse(null); // TODO: Handle properly, but should never happen anyway
-                        return new AbstractMap.SimpleEntry<>(game, config);
+                        return new ImmutablePair<>(game, config);
                     })
                     .collect(Collectors.partitioningBy(entry -> entry.getKey().phase().equals(GamePhase.SETUP)));
 
-            List<AbstractMap.SimpleEntry<Game, AutomaticPingConfig>> setupGamePairs = partitionedGameConfigPairs.get(true);
-            List<AbstractMap.SimpleEntry<Game, AutomaticPingConfig>> activeGamePairs = partitionedGameConfigPairs.get(false);
+            List<ImmutablePair<Game, AutomaticPingConfig>> setupGamePairs = partitionedGameConfigPairs.get(true);
+            List<ImmutablePair<Game, AutomaticPingConfig>> activeGamePairs = partitionedGameConfigPairs.get(false);
 
         } catch(IOException e) {
             System.err.println("Error reading directory: " + e.getMessage());
