84c34eec5d276e59b3bf9d63077d64802cf0d849
Aaron J Yoder <aaronjyoder@gmail.com>
2024-03-03 19:18:05 -0500

    Use per-game config model

--- src/main/java/com/riskrieg/bot/config/service/AutomaticPingConfig.java
+++ src/main/java/com/riskrieg/bot/config/service/AutomaticPingConfig.java
@@ -2,15 +2,15 @@ package com.riskrieg.bot.config.service;
 
 import com.riskrieg.bot.BotConstants;
 import com.riskrieg.bot.config.Configuration;
+import com.riskrieg.core.api.identifier.GameIdentifier;
 
 import java.nio.file.Path;
-import java.util.Set;
 
-public record AutomaticPingConfig(long guildId, Set<GamePingConfigData> gamePingConfigData) implements Configuration {
+public record AutomaticPingConfig(long guildId, GameIdentifier identifier, boolean enabled, boolean pingInterval, boolean lastPing) implements Configuration {
 
     @Override
     public Path path() {
-        return Path.of(BotConstants.CONFIG_PATH + "service/automatic-ping/config-" + guildId + ".json");
+        return Path.of(BotConstants.CONFIG_PATH + "service/automatic-ping/" + guildId + "/" + identifier.id() + ".json");
     }
 
 }
--- src/main/java/com/riskrieg/bot/config/service/GamePingConfigData.java
+++
@@ -1,6 +0,0 @@
-package com.riskrieg.bot.config.service;
-
-import com.riskrieg.core.api.identifier.GameIdentifier;
-
-public record GamePingConfigData(GameIdentifier identifier, boolean enabled, boolean pingInterval, boolean lastPing) {
-}
