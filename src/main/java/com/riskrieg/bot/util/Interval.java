e633d4068f60b9aa025eb85aaa71bd1b12d5a066
Aaron J Yoder <aaronjyoder@gmail.com>
2024-03-05 01:41:35 -0500

    Create Interval.java

---
+++ src/main/java/com/riskrieg/bot/util/Interval.java
@@ -0,0 +1,22 @@
+package com.riskrieg.bot.util;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.math.BigInteger;
+import java.util.concurrent.TimeUnit;
+
+public record Interval(long period, TimeUnit unit) implements Comparable<Interval> {
+
+    @Override
+    public int compareTo(@NotNull Interval o) {
+        BigInteger thisInterval = convert(this);
+        BigInteger thatInterval = convert(o);
+
+        return thisInterval.compareTo(thatInterval);
+    }
+
+    private BigInteger convert(Interval interval) {
+        return BigInteger.valueOf(interval.period()).multiply(BigInteger.valueOf(interval.unit().toNanos(1)));
+    }
+
+}
